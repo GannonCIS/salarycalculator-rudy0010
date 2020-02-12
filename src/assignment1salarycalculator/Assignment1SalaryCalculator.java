cd89e73a7b3a9f6065ed8244172aef32d437d16e
Jacob Rudy <rudy001@gannon.edu>
2020-02-12 16:07:00 -0500

    first revision added messages with reminders for project

--- src/assignment1salarycalculator/Assignment1SalaryCalculator.java
+++ src/assignment1salarycalculator/Assignment1SalaryCalculator.java
@@ -15,6 +15,13 @@ public class Assignment1SalaryCalculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //DUE FRIDAY? MAYBE MONDAY? WEDNESDAY PERHAPS?
+        //52 weeks of work per year
+        //40 hours per week
+        //calcSalary(int hourlyRate)                        return int
+        //calcHourly(int salary)                            return int
+        //calcTakeHomePerMonth(int salary, double taxRate)  return double
+        //calcTaxes(int salary, double taxes)               return double
         // TODO code application logic here
     }
     
